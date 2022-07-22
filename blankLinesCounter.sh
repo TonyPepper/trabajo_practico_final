725352cd1e06dacdf47842b7f5a323c4330e830e
TonyPepper <peroniantonioa@gmail.com>
2022-07-22 09:20:19 -0300

    carga actualiza de las soluciones

--- blankLinesCounter.sh
+++ blankLinesCounter.sh
@@ -3,8 +3,7 @@
 # Contador de líneas en blanco
 
 TEXTO=$1
-grep -E --line-number --with-filename '^$' $TEXTO > cantidad.txt
-CANT=$(cat cantidad.txt | wc -l)
 
-echo "El numero de linas vacías en $TEXTO es de $CANT"
+CANT=$(grep -c ^$ $TEXTO)
+echo "El numero de lineas vacías en $TEXTO es de $CANT"
 
--- statsUsageWords.sh
+++ statsUsageWords.sh
@@ -1,30 +1,18 @@
 #!/bin/bash
-
+#Indicador estadístico de uso de palabras, deben ser de al menos 4(cuatro) letras. (mostrar un Top Ten de estas palabras ordenadas desde la que tienemás apariciones a la que tiene menos). Es decir, filtrar las palabras quetengan al menos 4 letras y de éstas, elegir las 10 más usadas.
 TEXTO=$1
 cat textovacio.txt > mayor4.txt
 
-for word in $(cat $TEXTO)
+for word in $(cat $TEXTO | tr ",." " ")
 do
   if (( ${#word} >= 4 )); then
     echo $word >> mayor4.txt
    fi
-
+   
 done
 
-cat mayor4.txt | sort -u < mayor.txt 
-#if [ $count -eq 5 ];
-#then
-#break
-#fi
-#((count++)
-
-#for $(cat mayor.txt) in $(cat mayor4.txt)
-#do
-#  if ((  $(cat mayor.txt) in cat mayor4.txt )); then
-#    echo $word & echo >> mayor4.txt
-#   fi
-#
-#done
+echo "El top de palabras mayores de 4 letras es "
+echo "  VECES | PALABRA "
+cat mayor4.txt | sort | uniq -c | sort -gr | sed 10q 
 
 
-#cat mayor.txt | sort -u mayor.txt 
