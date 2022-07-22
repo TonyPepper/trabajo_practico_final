1e74c8fcee9f2ff01ab43fbd48bbdb2d4a99bb8e
TonyPepper <peroniantonioa@gmail.com>
2022-07-22 10:01:56 -0300

    Carga de ejercio 3

--- findNames.sh
+++ findNames.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+#Identificación de nombres propios (se identifican sólo si están en este formato Nnnnnnnnn), aunque la palabra no sea un nombre propio realmente.
+
 TEXTO=$1
-cat textovacio.txt > mayor4.txt
+echo "Las palabras empezadas con mayusculas son"
+for word in $(cat "$TEXTO" | tr ",." " ")
+  do  
+    if [[ ${word:0:1} == [A-Z] && ${word:2:1} == [a-z] ]]
+  then
+    echo "$word"
 
-for word in $(cat $TEXTO)
-do
-   echo $word >> mayor4.txt
+   fi
+   
 done
-
-cat mayor4.txt #| grep '${A,,*}'
