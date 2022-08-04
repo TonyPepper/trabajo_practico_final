faeceb165c7d289c1ce7ecc611791e8629ba6d14
TonyPepper <peroniantonioa@gmail.com>
2022-08-03 21:25:25 -0300

    cambio fuerzo con doble comillas la variable

--- menu.sh
+++ menu.sh
@@ -3,18 +3,15 @@ PS3='Elija> '
 echo "¿Que programa desea ejecutar?"
 select programa in "I.E. de longitud de palabras" "I.E. de uso de palabras" "Nombres propios" "I.E. de longitud de oraciones" "Contador de líneas en blanco" "SALIR" 
 do
-[ -e $programa ] && echo "Elegir opcion valida!" && continue
-[ $REPLY == 6 ] && echo "Hasta luego" && break
-[ $REPLY == 1 ] && /bin/bash statsWords.sh texto.txt
-[ $REPLY == 2 ] && /bin/bash statsUsageWords.sh texto.txt
-[ $REPLY == 3 ] && /bin/bash findNames.sh texto.txt
-[ $REPLY == 4 ] && /bin/bash statsSentences.sh texto.txt
-[ $REPLY == 5 ] && /bin/bash blankLinesCounter.sh texto.txt
+[ -e "$programa" ] && echo "Elegir opcion valida!" && continue
+[ "$REPLY" == 6 ] && echo "Hasta luego" && break
+[ "$REPLY" == 1 ] && /bin/bash statsWords.sh texto.txt
+[ "$REPLY" == 2 ] && /bin/bash statsUsageWords.sh texto.txt
+[ "$REPLY" == 3 ] && /bin/bash findNames.sh texto.txt
+[ "$REPLY" == 4 ] && /bin/bash statsSentences.sh texto.txt
+[ "$REPLY" == 5 ] && /bin/bash blankLinesCounter.sh texto.txt
 
 
 done
-
-echo "Continua el código...."
-
 
 exit 1
